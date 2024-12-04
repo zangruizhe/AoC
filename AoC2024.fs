1c01ce82e82137b1d0e98a6cd2b14242c5077c22
zangruizhe <zangruizhe@gmail.com>
2024-12-04 21:01:05 +0800

    add AoC2024 day1/2/3/4

--- AoC2024.fs
+++ AoC2024.fs
@@ -23,64 +23,180 @@ let DMAX = Double.MaxValue
 let DMIN = Double.MinValue
 let DNAN = Double.NaN
 let pi: double = 3.14159265358979323846
-
 let tmp_dict = Dictionary<int, int>()
 let tmp_set = HashSet<int>()
 
-let RedirectConsole () =
-    let if_stream = new FileStream("test.in", FileMode.OpenOrCreate, FileAccess.Read)
-    let sin = new StreamReader(if_stream)
-    Console.SetIn(sin)
-    
-    // let of_stream = new FileStream("test.out", FileMode.Create, FileAccess.Write)
-    // let sout = new StreamWriter(of_stream)
-    // sout.AutoFlush <- true
-    // Console.SetOut(sout)
+let split2Str (split: string) (src: string) : string[] =
+    src.Split(split, StringSplitOptions.RemoveEmptyEntries)
 
-let ReadInputV2 () : string[] =
-    let rec loop acc =
-        let line = Console.ReadLine()
-        if line <> null then loop (line :: acc) else acc
+let split2Int (src: string) : int[] = src |> split2Str " " |> Array.map int
 
-    loop [] |> List.rev |> List.toArray
-
-let ReadInput () : string[] =
-    let input = ResizeArray<string>()
-    let mutable line = Console.ReadLine()
-
-    while line <> null do
-        input.Add(line)
-        line <- Console.ReadLine()
-
-    input |> Seq.toArray
-
-
-// let Join<'t> (src: 't []) : string =
-//     src |> Array.map string |> String.concat " "
-
-let split2Str (src: string) : string[] = src.Split(" ")
-let split2Int (src: string) : int[] = src |> split2Str |> Array.map int
-let split2IntByReg (patten: string) (src:string) = 
+let splitByReg (patten: string) (src: string) =
     let reg = Regex.Matches(src, patten)
+    reg |> Seq.map (_.Groups) |> Seq.toArray
 
-    reg
-    |> Seq.map (fun m -> m.Groups[1].Value |> int)
-    |> Seq.toArray
+let split2IntByReg (patten: string) (src: string) =
+    let rst =
+        splitByReg patten src
+        |> Array.map (fun g -> g |> Seq.skip 1 |> Seq.map (fun g -> int g.Value) |> Seq.toArray)
 
-module Solve =
-    let Solution (lines: string[]) =
-        let nums = lines |> Array.map (split2IntByReg "(\d+)")
-        let l = ResizeArray<int>()
-        let r = ResizeArray<int>()
-        nums |> Array.iter(fun n ->
-                           l.Add(n[0])
-                           r.Add(n[1]))
+    rst
+
+module Day1 =
+    let l = ResizeArray<int>()
+    let r = ResizeArray<int>()
+
+    let init (lines) =
+        lines
+        |> Array.map (split2Int)
+        |> Array.iter (fun n ->
+            l.Add(n[0])
+            r.Add(n[1]))
+
         l.Sort()
         r.Sort()
-        (l, r) ||> Seq.map2 (fun l r -> r - l) |> Seq.sum
+
+    let Q1 (lines: string[]) =
+        init lines
+        (l, r) ||> Seq.map2 (fun l r -> abs (r - l)) |> Seq.sum
+
+    let Q2 (lines: string[]) =
+        init lines
+        let r_dict = Dictionary<int, int>()
+        r |> Seq.iter (fun n -> r_dict[n] <- r_dict.GetValueOrDefault(n) + 1)
+        l |> Seq.map (fun n -> n * r_dict.GetValueOrDefault(n)) |> Seq.sum
+
+module Day2 =
+    let init (lines: string[]) = lines |> Array.map split2Int
+
+    let check (nums: int[]) =
+        nums
+        |> Array.pairwise
+        |> Array.map (fun (l, r) -> r - l)
+        |> fun pair ->
+            (pair |> Array.forall (fun n -> 1 <= n && n <= 3)
+             || pair |> Array.forall (fun n -> -1 >= n && n >= -3))
+
+    let Q1 (lines: string[]) =
+        lines |> init |> Array.filter check |> Array.length
+
+    let Q2 (lines: string[]) =
+        let lines = lines |> init
+
+        let bad_lines =
+            lines
+            |> Array.filter (fun nums -> not (check nums))
+            |> Array.filter (fun nums ->
+                [| 0 .. nums.Length - 1 |]
+                |> Array.exists (fun i ->
+                    let new_nums = Array.removeAt i nums
+                    check new_nums)
+                |> not)
+
+        lines.Length - bad_lines.Length
+
+module Day3 =
+    let Q1 (lines: string[]) =
+        let str = String.Concat(lines)
+        str |> split2IntByReg "mul\((\d+),(\d+)\)" |> Array.sumBy (fun n -> n[0] * n[1])
+
+    let Q2 (lines: string[]) =
+        let str = String.Concat(lines)
+
+        Regex.Matches(str, @"mul\((\d+),(\d+)\)|don't\(\)|do\(\)")
+        |> Seq.fold
+            (fun (l, isEnable) m ->
+                if m.Value.StartsWith("mul") then
+                    if isEnable then
+                        l @ [ (int m.Groups[1].Value, int m.Groups[2].Value) ], isEnable
+                    else
+                        l, isEnable
+                elif m.Value = "do()" then
+                    l, true
+                elif m.Value = "don't()" then
+                    l, false
+                else
+                    failwith $"wrong patten m={m.Value}")
+            ([], true)
+        |> fst
+        |> Seq.sumBy (fun (l, r) -> l * r)
+
+module Day4 =
+    let Q1 (lines: string[]) =
+        let word = "XMAS"
+        let R = lines.Length
+        let C = lines[0].Length
+
+        let rec dfs i j n (l, r) =
+            if i < 0 || i >= R || j < 0 || j >= C then
+                false
+            else
+                match lines[i][j] = word[n], n = word.Length - 1 with
+                | true, true -> true
+                | false, _ -> false
+                | _ -> dfs (i + l) (j + r) (n + 1) (l, r)
 
 
-RedirectConsole()
+        let check i j =
+            [ (-1, -1); (-1, 0); (-1, 1); (0, -1); (0, 1); (1, -1); (1, 0); (1, 1) ]
+            |> List.filter (dfs i j 0)
+            |> List.length
+
+        let rst =
+            [ for i in 0 .. R - 1 do
+                  for j in 0 .. C - 1 do
+                      if lines[i][j] = word[0] then
+                          check i j
+              ]
+            |> List.sum
+
+        rst
+
+    let Q2 (lines: string[]) =
+        let word = "MAS"
+        let R = lines.Length
+        let C = lines[0].Length
+
+        let rec dfs i j n (l, r) =
+            if i < 0 || i >= R || j < 0 || j >= C then
+                false
+            else
+                match lines[i][j] = word[n], n = word.Length - 1 with
+                | true, true -> true
+                | false, _ -> false
+                | _ -> dfs (i + l) (j + r) (n + 1) (l, r)
+
+
+        let getMid i j =
+            [ (-1, -1); (-1, 1); (1, -1); (1, 1) ]
+            |> List.filter (dfs i j 0)
+            |> List.map (fun (l, r) -> (i + l), (j + r))
+
+        let rst =
+            [ for i in 0 .. R - 1 do
+                  for j in 0 .. C - 1 do
+                      if lines[i][j] = word[0] then
+                          yield! getMid i j ]
+            |> List.countBy id
+            |> List.filter (fun idx -> (snd idx) = 2)
+            |> List.length
+
+        rst
+
+
+module Day =
+    let init (lines: string[]) = lines |> Array.map split2Int
+
+    let Q1 (lines: string[]) =
+        let lines = init lines
+        0
+
+    let Q2 (lines: string[]) =
+        let lines = init lines
+        0
+
 let start = DateTime.Now
-ReadInput() |> Solve.Solution |>(fun x -> printfn $"{x}")
+let lines = File.ReadAllLines "test.in"
+lines |> Day4.Q1 |> (fun x -> printfn $"Q1={x}")
+lines |> Day4.Q2 |> (fun x -> printfn $"Q2={x}")
 printfn $"Execution time: %A{(DateTime.Now - start).TotalSeconds} seconds"
