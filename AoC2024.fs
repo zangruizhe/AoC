59c14acd1028dededdc6f1876849357e94370605
zangruizhe <zangruizhe@gmail.com>
2024-12-06 21:40:44 +0800

    Add AoC2024 Day6

--- AoC2024.fs
+++ AoC2024.fs
@@ -2,8 +2,6 @@
 open System.Collections.Generic
 open System.IO
 open System.Text.RegularExpressions
-open Microsoft.FSharp.Collections
-open NHamcrest
 
 type Index = int * int
 type ll = int64
@@ -222,6 +220,7 @@ module Day5 =
 
     let Q2 (lines: string[]) =
         let rules_dict, updates = init lines
+
         let rec fix_bad (nums: vi) (rst: int list) =
             if nums.Length = 0 then
                 rst
@@ -243,6 +242,70 @@ module Day5 =
         |> Array.map (fun nums -> fix_bad nums [])
         |> Array.sumBy (fun nums -> nums[nums.Length / 2])
 
+
+type Day6(lines: string[]) =
+    let R = lines.Length
+    let C = lines[0].Length
+
+    let getStart (lines: string[]) =
+        lines
+        |> Array.indexed
+        |> Array.pick (fun (i, s) ->
+            let j = s.IndexOf("^")
+            if j <> -1 then Some(i, j) else None)
+
+    let ops = [ (-1, 0); (0, 1); (1, 0); (0, -1) ]
+
+    let rec moving i j op (pos_set: HashSet<Index>) =
+        pos_set.Add((i, j)) |> ignore
+        let next_i = i + fst ops[op]
+        let next_j = j + snd ops[op]
+
+        if next_i < 0 || next_i >= R || next_j < 0 || next_j >= C then
+            pos_set
+        elif lines[next_i][next_j] = '#' then
+            moving i j ((op + 1) % 4) pos_set
+        else
+            moving next_i next_j op pos_set
+
+
+    member this.Q1(lines: string[]) =
+        let start: Index = getStart lines
+        let post_set = moving (fst start) (snd start) 0 (HashSet())
+        post_set.Count
+
+    member this.Q2(lines: string[]) =
+        let start: Index = getStart lines
+        let pos_set = moving (fst start) (snd start) 0 (HashSet())
+        pos_set.Remove(start) |> ignore
+
+        let rec checkLoop i j op (pos_set: HashSet<int * int * int>) =
+            let next_i = i + fst ops[op]
+            let next_j = j + snd ops[op]
+
+            if next_i < 0 || next_i >= R || next_j < 0 || next_j >= C then
+                false
+            elif lines[next_i][next_j] = '#' then
+                if pos_set.Add((i, j, op)) = false then
+                    true
+                else
+                    checkLoop i j ((op + 1) % 4) pos_set
+            else
+                checkLoop next_i next_j op pos_set
+
+
+        pos_set
+        |> Seq.filter (fun (i, j) ->
+            let old = lines[i]
+            let str = old.ToCharArray()
+            str[j] <- '#'
+            lines[i] <- String(str)
+            let rst = checkLoop (fst start) (snd start) 0 (HashSet())
+            lines[i] <- old
+            rst)
+        |> Seq.length
+
+
 module Day =
     let init (lines: string[]) = lines |> Array.map split2Int
 
@@ -254,8 +317,9 @@ module Day =
         let lines = init lines
         0
 
+
 let start = DateTime.Now
 let lines = File.ReadAllLines "test.in"
-lines |> Day5.Q1 |> (fun x -> printfn $"Q1={x}")
-lines |> Day5.Q2 |> (fun x -> printfn $"Q2={x}")
+lines |> Day6(lines).Q1 |> (fun x -> printfn $"Q1={x}")
+lines |> Day6(lines).Q2 |> (fun x -> printfn $"Q2={x}")
 printfn $"Execution time: %A{(DateTime.Now - start).TotalSeconds} seconds"
