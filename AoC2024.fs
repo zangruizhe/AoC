7d935db02b19aeb9f74a497512626d4070af30eb
zangruizhe <zangruizhe@gmail.com>
2024-12-08 20:39:36 +0800

    add AoC2024 Day8

--- AoC2024.fs
+++ AoC2024.fs
@@ -352,6 +352,84 @@ type Day7(lines: string[]) =
         |> Array.filter canEvaluated
         |> Array.sumBy (fun nums -> nums[0])
 
+type Day8(lines: string[]) =
+    let R = lines.Length
+    let C = lines[0].Length
+
+    let getPoints (lines: string[]) : Map<char, Index array> =
+        let points =
+            [| for i in 0 .. R - 1 do
+                   for j in 0 .. C - 1 do
+                       if lines[i][j] <> '.' then
+                           (lines[i][j], (i, j)) |]
+
+        points
+        |> Array.groupBy fst
+        |> Map.ofArray
+        |> Map.map (fun _ v -> v |> Array.map snd)
+
+
+    let inBoard (i, j) : bool = 0 <= i && i < R && 0 <= j && j < C
+
+    let getIdxPair (idx_list: Index array) =
+        [ for i in 0 .. idx_list.Length - 2 do
+              for j in i + 1 .. idx_list.Length - 1 do
+                  (idx_list[i], idx_list[j]) ]
+
+    member this.Q1() =
+        let points_dict = lines |> getPoints
+
+        let getAntinodes (a: Index) (b: Index) : Index list =
+            let ar, ac = a
+            let br, bc = b
+
+            let x = abs (ar - br)
+            let y = abs (ac - bc)
+
+            match ar < br, ac < bc with
+            | true, true -> [ (ar - x, ac - y); (br + x, bc + y) ]
+            | true, false -> [ (ar - x, ac + y); (br + x, bc - y) ]
+            | false, true -> [ (ar + x, ac - y); (br - x, bc + y) ]
+            | false, false -> [ (ar + x, ac + y); (br - x, bc - y) ]
+
+        points_dict.Values
+        |> Array.ofSeq
+        |> Seq.collect (fun idx_list -> idx_list |> getIdxPair |> List.collect (fun (l, r) -> getAntinodes l r))
+        |> Seq.filter inBoard
+        |> Set.ofSeq
+        |> Seq.length
+
+    member this.Q2() =
+        let points_dict = lines |> getPoints
+
+        let getAntinodesInLine (ar, ac) x y =
+            let rec loop (ar, ac) x y rst =
+                if not (inBoard (ar, ac)) then
+                    rst
+                else
+                    loop (ar + x, ac + y) x y ((ar, ac) :: rst)
+
+            loop (ar, ac) x y []
+
+        let getAntinodes (a: Index) (b: Index) : Index list =
+            let ar, ac = a
+            let br, bc = b
+
+            let x = abs (ar - br)
+            let y = abs (ac - bc)
+
+            match ar < br, ac < bc with
+            | true, true -> getAntinodesInLine a -x -y @ getAntinodesInLine b x y
+            | true, false -> getAntinodesInLine a -x y @ getAntinodesInLine b x -y
+            | false, true -> getAntinodesInLine a x -y @ getAntinodesInLine b -x y
+            | false, false -> getAntinodesInLine a x y @ getAntinodesInLine b -x -y
+
+        points_dict.Values
+        |> Array.ofSeq
+        |> Seq.collect (fun idx_list -> idx_list |> getIdxPair |> List.collect (fun (l, r) -> getAntinodes l r))
+        |> Set.ofSeq
+        |> Seq.length
+
 
 type Day(lines: string[]) =
     member this.Q1() = 0
@@ -361,6 +439,6 @@ type Day(lines: string[]) =
 
 let start = DateTime.Now
 let lines = File.ReadAllLines "test.in"
-Day7(lines).Q1() |> (fun x -> printfn $"Q1={x}")
-Day7(lines).Q2() |> (fun x -> printfn $"Q2={x}")
+Day8(lines).Q1() |> (fun x -> printfn $"Q1={x}")
+Day8(lines).Q2() |> (fun x -> printfn $"Q2={x}")
 printfn $"Execution time: %A{(DateTime.Now - start).TotalSeconds} seconds"
